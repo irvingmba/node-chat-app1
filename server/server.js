39a1646333c69641bb2bd560aed3fdb3afaf238b
martim <imbedoya@tekmexico.com>
2019-09-12 15:31:19 -0700

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,23 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'John',
+        text: 'See you then',
+        createdAt: 123123
+    });
+
+    socket.emit('newEmail', {
+        from: 'martin@example.com',
+        text: 'Hey. What is going on.',
+        createdAt: 123
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log("createMessage", message);
+    });
+    
     socket.on('disconnect', () => {
         console.log("User was disconnected");
     });
